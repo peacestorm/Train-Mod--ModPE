7e36b4c7d16d37e300ebf68cd788c9b465fc005f
alaphant42 <alaphant42@gmail.com>
2015-02-04 21:58:38 -0600

    Added code
    first version

--- train.js
+++ train.js
@@ -1,1 +1,19 @@
-coming soon
+ModPE.setItem(500,"minecart_normal",0,"Train")
+Player.addItemCreativeInv(500,1,0);
+
+var trainRenderer = Renderer.createHumanoidRenderer();
+var trainModel = trainRenderer.getModel();
+
+/*var leftLeg = trainModel.getPart("leftLeg");
+var rightLeg = trainModel.getPart("rightLeg");
+leftLeg.clear();
+leftLeg.setTextureOffset(18, 4, false);
+leftLeg.addBox(-6, -10, -7, 12, 18, 10, 0);*/
+
+function useItem(x,y,z,i,b) {
+var rail = getTile(x,y,z)
+if(i == 500) {
+if(rail == 66||rail == 27) {
+var Train = Level.spawnMob(x,y,z,84);
+Entity.setRenderType(Train,trainRenderer);
+}}}
