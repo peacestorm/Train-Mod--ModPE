509c2ba2cc0fcb97e4cc276900ca2d1ac24e3c25
alaphant42 <alaphant42@gmail.com>
2015-02-08 17:59:43 -0600

    Gui
    Addded Some Gui

--- train.js
+++ train.js
@@ -20,3 +20,43 @@ var TrainMob = Level.spawnMob(x,y,z,15, "mob/Train.png"); //changed to 15 for vi
 // you also need to make another one for the cart and name it "TrainCart.png".  Put it in the same folder.  Make sure it is transparent.
 Entity.setRenderType(TrainMob,trainRenderer.renderType);
 }}}
+
+var buttonWindow = null;  
+
+function  newLevel() {
+  var activity = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+  activity.runOnUiThread(new java.lang.Runnable({ run: function() {
+        try {
+          buttonWindow = new android.widget.PopupWindow();
+          var layout = new android.widget.RelativeLayout(activity);
+          var button = new android.widget.Button(activity);
+          button.setText("Remove Train");
+          button.setOnClickListener(new android.view.View.OnClickListener({
+                onClick: function(viewarg) {
+                  Entity.remove(TrainMob);
+                  Entity.remove(TrainCart);
+                  clientMessage("Train Removed!");
+                }
+          }));
+          layout.addView(button);
+          buttonWindow.setContentView(layout);
+          buttonWindow.setWidth(android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT);
+          buttonWindow.setHeight(android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT);
+          buttonWindow.setBackgroundDrawable(new
+android.graphics.drawable.ColorDrawable(android.graphics.Color.TRANSPARENT));
+          buttonWindow.showAtLocation(activity.getWindow().getDecorView(), android.view.Gravity.RIGHT | android.view.Gravity.BOTTOM, 0, 0);
+        }catch(problem){
+          print("Button could not be displayed: " + problem);
+        }
+  }}));
+}
+
+function leaveGame() {
+  var activity = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+  activity.runOnUiThread(new java.lang.Runnable({ run: function() {
+        if(buttonWindow != null) { 
+          buttonWindow.dismiss(); 
+          buttonwindow = null;
+        }
+  }}));
+}
